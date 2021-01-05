1be8e6bc042eb1df2eb44b3eca769c566a081f57
shirasa <n3h_224@yahoo.co.jp>
2021-01-05 03:09:40 +0000

    kadai_14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,8 +15,20 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request,Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+            // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+ 
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+        
         return redirect('admin/profile/create');
     }
     
